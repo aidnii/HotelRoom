9d59dd37d3bcb8bd030b866eec5003d5ad955ff2
cryptichic <syberchief@gmail.com>
2022-06-13 17:56:35 -0500

    added constructor and book function

--- HotelRoom.sol
+++ HotelRoom.sol
@@ -3,5 +3,13 @@ pragma solidity ^0.8.0;
 
 //Creating a hotel room smart contract
 contract HotelRoom {
-    
+    address payable owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function book() {
+        owner.transfer(msg.value);
+    }
 }
